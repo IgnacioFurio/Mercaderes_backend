84326b145f09ae641da4cab2a96c180b7b2cb9ee
Ignacio Furió José <bichoifj@gmail.com>
2026-04-28 13:03:17 +0200

    Add Merchant model and migration

---
+++ migrations/20260428110113-create-merchant.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Merchants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      species: {
+        type: Sequelize.STRING
+      },
+      shopType: {
+        type: Sequelize.STRING
+      },
+      region: {
+        type: Sequelize.STRING
+      },
+      quality: {
+        type: Sequelize.STRING
+      },
+      attitude: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Merchants');
+  }
+};
---
+++ models/merchant.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Merchant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Merchant.init({
+    name: DataTypes.STRING,
+    species: DataTypes.STRING,
+    shopType: DataTypes.STRING,
+    region: DataTypes.STRING,
+    quality: DataTypes.STRING,
+    attitude: DataTypes.STRING,
+    notes: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Merchant',
+  });
+  return Merchant;
+};
